7735f4098451c07ecfd9ae658b925e0fd873fa48
greyfenrir <grey.fenrir@gmail.com>
2022-08-09 14:17:51 +0300

    add Shipment model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Shipment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    from_addr = models.TextField(default='')
+    to_addr = models.TextField(default='')
+
+    # Receiving, Processing, Fulfilling
+    state = models.TextField(default='Receiving')   # todo: convert to ChoiceField
+
+    owner = models.ForeignKey('auth.User', related_name='shipments', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created']
