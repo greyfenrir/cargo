78427d7ed42065bcb2b72ea94522cb830ab5897c
greyfenrir <grey.fenrir@gmail.com>
2022-08-09 14:23:31 +0300

    serialize shipments

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Shipment
+
+
+class ShipmentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Shipment
+        fields = ['id', 'created', 'from_addr', 'to_addr', 'state', 'owner']
 
 
 class UserSerializer(serializers.ModelSerializer):
+    shipments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'shipments']
