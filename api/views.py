f0367659940fb453b3da08ffa2457c345ea5b884
greyfenrir <grey.fenrir@gmail.com>
2022-08-09 14:27:20 +0300

    add shipment view

--- api/views.py
+++ api/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-from . import serializers
 from django.contrib.auth.models import User
+
+from . import serializers
+from .models import Shipment
 
 
 class UserList(generics.ListAPIView):
@@ -14,3 +16,16 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class ShipmentList(generics.ListCreateAPIView):
+    queryset = Shipment.objects.all()
+    serializer_class = serializers.ShipmentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ShipmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Shipment.objects.all()
+    serializer_class = serializers.ShipmentSerializer
