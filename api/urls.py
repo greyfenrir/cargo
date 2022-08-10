892fb39e9622cf406271a5581abd8e3b39fae3ce
greyfenrir <grey.fenrir@gmail.com>
2022-08-10 19:56:47 +0300

    add name to urls for testing purpose

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('shipments/', views.ShipmentList.as_view()),
-    path('shipments/<int:pk>/', views.ShipmentDetail.as_view()),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('shipments/', views.ShipmentList.as_view(), name='shipment-list'),
+    path('shipments/<int:pk>/', views.ShipmentDetail.as_view(), name='shipment-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
