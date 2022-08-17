6d30404c53488ab2f5e3958aa3a03dd2aa63a926
greyfenrir <grey.fenrir@gmail.com>
2022-08-17 12:14:30 +0300

    add components and views

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -8,17 +8,17 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/shipments',
+    name: 'Shipments',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/Shipments.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
 
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Cargo logo" src="../assets/logo.png">
+    <Hello msg="Welcome to Cargo App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Hello from '@/components/Hello.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Hello
   }
 }
 </script>
