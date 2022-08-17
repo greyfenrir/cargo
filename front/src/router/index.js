d108bdf2d2459b6a1f1c27e3e4a9b2e87e2cdc25
greyfenrir <grey.fenrir@gmail.com>
2022-08-17 10:22:32 +0300

    add front template

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-//import ShipmentById from '@/components/ShipmentById'
-//import AllShipments from '@/components/AllShipments'
 
 const routes = [
   {
@@ -16,10 +14,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-//    { path: '/shipments/:id', component: ShipmentById },
-//  { path: '/shipments/', component: AllShipments },
-
+  }
 ]
 
 const router = createRouter({
